8baae3fca5b0f84f689f392322576d148e5a1830
James Chong <wei3hua2@gmail.com>
2018-07-15 09:15:46 +0800

    enhance the build process

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  target: 'node',
+  output: {
+    path: path.join(__dirname, '.'),
+    filename: 'build/index.js'
+  }
+}
