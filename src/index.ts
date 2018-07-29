ff37486baad06f7954c5082e54a51d51f502e00d
James Chong <wei3hua2@gmail.com>
2018-07-29 14:29:36 +0800

    expose ramda

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {RpsContext} from './context';
 import {RpsModule,rpsAction,rpsActionSkipErrHandling} from './decorators';
 import {RpsModuleModel,RpsModuleActionsModel,RpsActionModel,RpsActionParamModel,RpsDefaultModel} from './interface';
+import * as R from 'ramda';
 
 export {RpsContext,RpsModule,rpsAction,rpsActionSkipErrHandling,RpsDefaultModel,
-    RpsModuleModel,RpsModuleActionsModel,RpsActionModel,RpsActionParamModel};
+    RpsModuleModel,RpsModuleActionsModel,RpsActionModel,RpsActionParamModel,R};
